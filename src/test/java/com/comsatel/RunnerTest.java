f002524223b32fd368f6094f83c5bb915039dfa8
jjhsolutions4 <esancho@controlaya.com>
2024-08-13 11:59:53 -0500

    evitar mensaje - chrome controlado por sowtware de automatizacion

--- src/test/java/com/comsatel/RunnerTest.java
+++ src/test/java/com/comsatel/RunnerTest.java
@@ -5,6 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,9 +21,13 @@ import java.util.concurrent.TimeUnit;
         })
 public class RunnerTest {
         public static final WebDriver driver ;
-
+        
+        public static final ChromeOptions options = new ChromeOptions();
+         
         static {
-                driver = new ChromeDriver();
+	        	options.setExperimentalOption("useAutomationExtension", false);
+	            options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+                driver = new ChromeDriver(options);
                 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
                 driver.manage().window().maximize();
         }
